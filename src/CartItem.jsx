90874d3ec96611dc1717fa02fedfd8c8bab056c3
harshshinde26 <email@example.com>
2025-03-07 02:24:00 -0500

    PreProdChanges

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,13 +31,18 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
+    if (item){
+        dispatch(updateQuantity({name : item.name, quantity: item.quantity + 1}));
+    }
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({name : item.name, quantity: item.quantity - 1}));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}));
+    
   };
 
   // Calculate total cost based on quantity for an item
